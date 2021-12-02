1f18e6372deb0049256b256554839097eb60d903
hyeon <couse6610@gmail.com>
2021-12-02 23:40:23 +0900

    Update : typeOrm + env + HttpExceptionFilter + LoggerMiddleware

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,51 @@
-import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { LoggerMiddleware } from './middlewares/logger.middleware';
+import { UserModule } from './user/user.module';
+import { Users } from './entities/Users';
+import { APP_FILTER } from '@nestjs/core';
+import { HttpExceptionFilter } from './common/exception/http-exception.filter';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath:
+        process.env.NODE_ENV === 'production' ? '.env.prod' : '.env.dev',
+    }),
+    TypeOrmModule.forRoot({
+      type: process.env.DB_TYPE as any,
+      host: process.env.DB_HOST,
+      port: process.env.DB_PORT as any,
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_DATABASE,
+      entities: [Users],
+      migrations: [__dirname + '/src/migrations/*.ts'],
+      cli: { migrationsDir: 'src/migrations' },
+      autoLoadEntities: true,
+      charset: 'utf8mb4',
+      synchronize: false,
+      logging: true,
+      keepConnectionAlive: true,
+    }),
+    UserModule,
+    TypeOrmModule.forFeature([Users]),
+  ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [
+    AppService,
+    {
+      provide: APP_FILTER,
+      useClass: HttpExceptionFilter,
+    },
+  ],
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer): any {
+    consumer.apply(LoggerMiddleware).forRoutes('*');
+  }
+}
