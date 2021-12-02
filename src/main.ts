2df38243e31be9f1874f78006f711904931e4c5d
hyeon <couse6610@gmail.com>
2021-12-02 23:41:24 +0900

    Update : ValidationPipe + HttpExceptionFilter + webpack

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,31 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { HttpExceptionFilter } from './common/exception/http-exception.filter';
+import { ValidationPipe } from '@nestjs/common';
+
+declare const module: any;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const port = process.env.PORT || 8080;
+  app.useGlobalPipes(new ValidationPipe());
+  app.useGlobalFilters(new HttpExceptionFilter());
+  // const config = new DocumentBuilder()
+  //   .setTitle('동네잡일 API')
+  //   .setDescription('동네잡일 개발을 위한 API 문서입니다.')
+  //   .setVersion('1.0')
+  //   .addCookieAuth('connect.sid')
+  //   .build();
+  // const document = SwaggerModule.createDocument(app, config);
+  // SwaggerModule.setup('api', app, document);
+
+  await app.listen(port);
+  console.log(`App listening on the port ${port}`);
+
+  if (module.hot) {
+    module.hot.accept();
+    module.hot.dispose(() => app.close());
+  }
 }
 bootstrap();
