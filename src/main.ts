ac9312d4bb25500ed7064c8ca79fab5493c76703
hyeon <couse6610@gmail.com>
2021-12-05 20:47:39 +0900

    Update : Filter 수정

--- src/main.ts
+++ src/main.ts
@@ -1,17 +1,21 @@
-import { NestFactory } from '@nestjs/core';
+import { HttpAdapterHost, NestFactory, Reflector } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
-import { HttpExceptionFilter } from './common/exception/http-exception.filter';
+import { HttpExceptionFilter } from './filter/http-exception.filter';
 import { ValidationPipe } from '@nestjs/common';
-import { QueryFailedFilter } from './common/exception/query-failed.filter';
+import { QueryFailedFilter } from './filter/query-failed.filter';
 
 declare const module: any;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const port = process.env.PORT || 8080;
+  const reflector = app.get(Reflector);
   app.useGlobalPipes(new ValidationPipe());
-  app.useGlobalFilters(new HttpExceptionFilter(), new QueryFailedFilter());
+  app.useGlobalFilters(
+    new HttpExceptionFilter(),
+    new QueryFailedFilter(reflector),
+  );
   // const config = new DocumentBuilder()
   //   .setTitle('동네잡일 API')
   //   .setDescription('동네잡일 개발을 위한 API 문서입니다.')
