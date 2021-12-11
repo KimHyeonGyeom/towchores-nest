ca6c4bd1b914c578e656cddaaf9578ae0b54b24c
hyeon <couse6610@gmail.com>
2021-12-11 19:17:00 +0900

    Add : 회원가입 dto 추가

---
+++ src/user/dto/signup.request.dto.ts
@@ -0,0 +1,10 @@
+import { Users } from '../../entities/Users';
+import { ApiProduces, PickType } from '@nestjs/swagger';
+
+export class SignupRequestDto extends PickType(Users, [
+  'nickname',
+  'social_id',
+  'social_type',
+  'latitude',
+  'longitude',
+] as const) {}
