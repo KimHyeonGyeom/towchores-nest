7e9e88643eb4d032efb7e665e64824c37d641d4c
hyeon <couse6610@gmail.com>
2021-12-12 23:42:41 +0900

    Fix : response 메시지 변경

--- src/interceptors/undefinedToNull.interceptor.ts
+++ src/interceptors/undefinedToNull.interceptor.ts
@@ -7,14 +7,19 @@ import {
 import { map, Observable } from 'rxjs';
 
 @Injectable()
+//json에서는 undefined를 인식하지 못하기 때문에 null로 치환
 export class UndefinedToNullInterceptor implements NestInterceptor {
   intercept(
     context: ExecutionContext,
     next: CallHandler<any>,
   ): Observable<any> | Promise<Observable<any>> {
-    //json에서는 undefined를 인식하지 못하기 때문에 null로 치환
+    //전 부분 (시간 초 잴때 사용)
     return next
       .handle()
-      .pipe(map((data) => (data === undefined ? null : data)));
+      .pipe(
+        map((data) =>
+          data === undefined ? null : { success: true, response: data },
+        ),
+      );
   }
 }
